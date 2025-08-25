4ec9afb37259b52b0502cafce18b2e5e8b0b58dd
bunnygirlHyacinth <xxst4rf11m00nb700db4nnyxx@gmail.com>
2025-08-25 15:53:18 -0700

    added scramble cipher to the power pool. applies psionic insulation for 30 seconds.

---
+++ Content.Server/Abilities/Psionics/Abilities/ScrambleCipherPowerSystem.cs
@@ -0,0 +1,31 @@
+﻿
+using Content.Shared.Abilities.Psionics;
+using Content.Shared.Actions.Events;
+using Content.Shared.StatusEffect;
+
+namespace Content.Server.Abilities.Psionics
+{
+    public sealed class ScrambleCipherPowerSystem : EntitySystem
+    {
+        [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
+        [Dependency] private readonly StatusEffectsSystem _statusEffectsSystem = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<ScrambleCipherPowerActionEvent>(OnPowerUsed);
+        }
+
+        private void OnPowerUsed(ScrambleCipherPowerActionEvent args)
+        {
+            if (!_psionics.OnAttemptPowerUse(args.Performer, args.Target, "scramble cipher", false))
+                return;
+
+
+            _statusEffectsSystem.TryAddStatusEffect(args.Target, "Scrambled", TimeSpan.FromSeconds(10), false, "BackwardsAccent");
+            _statusEffectsSystem.TryAddStatusEffect(args.Target, "PsionicallyInsulated", TimeSpan.FromSeconds(30), false, "PsionicInsulation");
+            _psionics.LogPowerUsed(args.Performer, "scramble cipher");
+            args.Handled = true;
+        }
+    }
+}
